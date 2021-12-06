6ce5817d2fd7dd8e563495c7705c511ea6b88550
kai-wen-yang <92202424+kai-wen-yang@users.noreply.github.com>
2021-12-06 22:27:18 +0800

    Update disentangle_cifar.sh

--- toolkits/disentangle_cifar.sh
+++ toolkits/disentangle_cifar.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python tools/disentangle_cifar.py --save_dir results/disentangle_cifar_ce0.2 --dim 2048 --fdim 32 --ce 0.2 --optim cosine
+CUDA_VISIBLE_DEVICES=0 python tools/disentangle_cifar.py --save_dir results/disentangle_cifar_ce0.2  --ce 0.2 --optim cosine
