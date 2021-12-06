cc8b8bb7427fe5df8c74a77eada89a8fb09d2d50
kai-wen-yang <92202424+kai-wen-yang@users.noreply.github.com>
2021-12-06 23:07:01 +0800

    Update detection.sh

--- detection/detection.sh
+++ detection/detection.sh
@@ -1,13 +1,13 @@
-CUDA_VISIBLE_DEVICES=1 python ADV_Samples_Subspace.py --dataset cifar10 --net_type resnet --adv_type FGSM --gpu 0 --outf ./data/disentangle_cifar_ce1/ --vae_path ../results/disentangle_cifar_ce1/model_epoch291.pth;
-CUDA_VISIBLE_DEVICES=1 python ADV_Samples_Subspace.py --dataset cifar10 --net_type resnet --adv_type BIM --gpu 0 --outf ./data/disentangle_cifar_ce1/ --vae_path ../results/disentangle_cifar_ce1/model_epoch291.pth;
-CUDA_VISIBLE_DEVICES=1 python ADV_Samples_Subspace.py --dataset cifar10 --net_type resnet --adv_type PGD --gpu 0 --outf ./data/disentangle_cifar_ce1/ --vae_path ../results/disentangle_cifar_ce1/model_epoch291.pth;
-CUDA_VISIBLE_DEVICES=1 python ADV_Samples_Subspace.py --dataset cifar10 --net_type resnet --adv_type CW --gpu 0 --outf ./data/disentangle_cifar_ce1/ --vae_path ../results/disentangle_cifar_ce1/model_epoch291.pth;
-CUDA_VISIBLE_DEVICES=1 python ADV_Samples_Subspace.py --dataset cifar10 --net_type resnet --adv_type PGD-L2 --gpu 0 --outf ./data/disentangle_cifar_ce1/ --vae_path ../results/disentangle_cifar_ce1/model_epoch291.pth;
+CUDA_VISIBLE_DEVICES=1 python ADV_Samples_Subspace.py --dataset cifar10 --net_type resnet --adv_type FGSM --gpu 0 --outf ./data/cd-vae-1/ --vae_path ../pretrained/cd-vae-1.pth;
+CUDA_VISIBLE_DEVICES=1 python ADV_Samples_Subspace.py --dataset cifar10 --net_type resnet --adv_type BIM --gpu 0 --outf ./data/cd-vae-1/ --vae_path ../pretrained/cd-vae-1.pth;
+CUDA_VISIBLE_DEVICES=1 python ADV_Samples_Subspace.py --dataset cifar10 --net_type resnet --adv_type PGD --gpu 0 --outf ./data/cd-vae-1/ --vae_path ../pretrained/cd-vae-1.pth;
+CUDA_VISIBLE_DEVICES=1 python ADV_Samples_Subspace.py --dataset cifar10 --net_type resnet --adv_type CW --gpu 0 --outf ./data/cd-vae-1/ --vae_path ../pretrained/cd-vae-1.pth;
+CUDA_VISIBLE_DEVICES=1 python ADV_Samples_Subspace.py --dataset cifar10 --net_type resnet --adv_type PGD-L2 --gpu 0 --outf ./data/cd-vae-1/ --vae_path ../pretrained/cd-vae-1.pth;
 
-CUDA_VISIBLE_DEVICES=1 python ADV_Generate_Mahalanobis_Subspace.py --dataset cifar10 --net_type resnet --adv_type FGSM --gpu 0 --outf ./data/disentangle_cifar_ce1/ --vae_path ../results/disentangle_cifar_ce1/model_epoch291.pth;
-CUDA_VISIBLE_DEVICES=1 python ADV_Generate_Mahalanobis_Subspace.py --dataset cifar10 --net_type resnet --adv_type BIM --gpu 0 --outf ./data/disentangle_cifar_ce1/ --vae_path ../results/disentangle_cifar_ce1/model_epoch291.pth;
-CUDA_VISIBLE_DEVICES=1 python ADV_Generate_Mahalanobis_Subspace.py --dataset cifar10 --net_type resnet --adv_type PGD --gpu 0 --outf ./data/disentangle_cifar_ce1/ --vae_path ../results/disentangle_cifar_ce1/model_epoch291.pth;
-CUDA_VISIBLE_DEVICES=1 python ADV_Generate_Mahalanobis_Subspace.py --dataset cifar10 --net_type resnet --adv_type CW --gpu 0 --outf ./data/disentangle_cifar_ce1/ --vae_path ../results/disentangle_cifar_ce1/model_epoch291.pth;
-CUDA_VISIBLE_DEVICES=1 python ADV_Generate_Mahalanobis_Subspace.py --dataset cifar10 --net_type resnet --adv_type PGD-L2 --gpu 0 --outf ./data/disentangle_cifar_ce1/ --vae_path ../results/disentangle_cifar_ce1/model_epoch291.pth;
+CUDA_VISIBLE_DEVICES=1 python ADV_Generate_Mahalanobis_Subspace.py --dataset cifar10 --net_type resnet --adv_type FGSM --gpu 0 --outf ./data/cd-vae-1/ --vae_path ../pretrained/cd-vae-1.pth;
+CUDA_VISIBLE_DEVICES=1 python ADV_Generate_Mahalanobis_Subspace.py --dataset cifar10 --net_type resnet --adv_type BIM --gpu 0 --outf ./data/cd-vae-1/ --vae_path ../pretrained/cd-vae-1.pth;
+CUDA_VISIBLE_DEVICES=1 python ADV_Generate_Mahalanobis_Subspace.py --dataset cifar10 --net_type resnet --adv_type PGD --gpu 0 --outf ./data/cd-vae-1/ --vae_path ../pretrained/cd-vae-1.pth;
+CUDA_VISIBLE_DEVICES=1 python ADV_Generate_Mahalanobis_Subspace.py --dataset cifar10 --net_type resnet --adv_type CW --gpu 0 --outf ./data/cd-vae-1/ --vae_path ../pretrained/cd-vae-1.pth;
+CUDA_VISIBLE_DEVICES=1 python ADV_Generate_Mahalanobis_Subspace.py --dataset cifar10 --net_type resnet --adv_type PGD-L2 --gpu 0 --outf ./data/cd-vae-1/ --vae_path ../pretrained/cd-vae-1.pth;
 
-CUDA_VISIBLE_DEVICES=1 python ADV_Regression_Subspace.py --net_type resnet --outf ./data/disentangle_cifar_ce1/;
+CUDA_VISIBLE_DEVICES=1 python ADV_Regression_Subspace.py --net_type resnet --outf ./data/cd-vae-1/;
