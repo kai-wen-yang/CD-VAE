49ec0baa18095d47e94ad4b41f8093ebe61e5d68
kai-wen-yang <92202424+kai-wen-yang@users.noreply.github.com>
2021-12-06 22:26:28 +0800

    Update disentangle_cifar.sh

--- toolkits/disentangle_cifar.sh
+++ toolkits/disentangle_cifar.sh
@@ -1,8 +1,1 @@
 CUDA_VISIBLE_DEVICES=0 python tools/disentangle_cifar.py --save_dir results/disentangle_cifar_ce0.2 --dim 2048 --fdim 32 --ce 0.2 --optim cosine
-
-
-
-#CUDA_VISIBLE_DEVICES=0 python tools/disentangle_cifar.py --save_dir results/disentangle_cifar_ce1 --dim 2048 --fdim 32 --ce 0.1 --optim consine
-#
-#CUDA_VISIBLE_DEVICES=0 python tools/disentangle_cifar.py --save_dir results/disentangle_cifar_ce0.2_curriculum_entropy --dim 2048 --fdim 32 --ce 0.2 --re 1 --kl 0.2 --optim consine --curriculum
-#
