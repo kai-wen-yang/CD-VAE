b17d3145ab1685b200ddbd4b4350e938a57f3ed7
Kaiwen Yang <92202424+kai-wen-yang@users.noreply.github.com>
2021-12-21 12:33:21 +0800

    Update VQ-VAE

--- networks/vae.py
+++ networks/vae.py
@@ -224,3 +224,65 @@ class CVAE_cifar(AbstractAutoEncoder):
             return out, hi, xi, mu, logvar
         else:
             return xi
+        
+class CVAE_imagenet(nn.Module):
+    def __init__(self, d, k=10, num_classes=9, num_channels=3, **kwargs):
+        super(CVAE_imagenet, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(num_channels, d, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(d),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(d, d, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(d),
+            nn.LeakyReLU(inplace=True),
+            ResBlock(d, d),
+            nn.BatchNorm2d(d),
+            ResBlock(d, d),
+            nn.BatchNorm2d(d),
+        )
+        self.decoder = nn.Sequential(
+            ResBlock(d, d),
+            nn.BatchNorm2d(d),
+            ResBlock(d, d),
+            nn.ConvTranspose2d(d, d, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(d),
+            nn.LeakyReLU(inplace=True),
+            nn.ConvTranspose2d(d, num_channels, kernel_size=4, stride=2, padding=1),
+        )
+        self.d = d
+        self.emb = NearestEmbed(k, d)
+
+        for l in self.modules():
+            if isinstance(l, nn.Linear) or isinstance(l, nn.Conv2d):
+                l.weight.detach().normal_(0, 0.02)
+                torch.fmod(l.weight, 0.04)
+                nn.init.constant_(l.bias, 0)
+
+        self.encoder[-1].weight.detach().fill_(1 / 40)
+
+        self.emb.weight.detach().normal_(0, 0.02)
+        torch.fmod(self.emb.weight, 0.04)
+
+        self.classifier = resnet50(pretrained=True, num_classes=num_classes)
+
+        self.L_bn = nn.BatchNorm2d(num_channels)
+
+    def encode(self, x):
+        return self.encoder(x)
+
+    def decode(self, x):
+        return torch.tanh(self.decoder(x))
+
+    def forward(self, x):
+
+        z_e = self.encode(x)
+
+        z_q, _ = self.emb(z_e, weight_sg=True)
+        emb, _ = self.emb(z_e.detach())
+
+        l = self.decode(z_q)
+        gx = self.L_bn(l)
+        out = self.classifier(x-xi)
+
+        return  out, gx, z_e, emb
